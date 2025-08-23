029e7294ff6120b893cefc31822af597f4dbb5b7
Pattaphon-sr <zenotadeo@gmail.com>
2025-08-23 15:37:03 +0700

    fix(controllers/auth.js): edit function login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,34 @@
 const { getConnection } = require("../config/db.js");
 const bcrypt = require("bcrypt");
 
-async function login(req, res) {}
+async function login(req, res) {
+  const { username, password } = req.body;
+  const sql = "SELECT * FROM `users` WHERE username = ?";
+
+  const con = await getConnection();
+  
+  con.query(sql, username, (err, results) => {
+    if (err) {
+      return res.status(500).send("Database error");
+    }
+    if (results.length != 1) {
+      return res.status(401).send("wrong username");
+    }
+    bcrypt.compare(password, results[0].password, (err, same) => {
+      if (err) {
+        return res.status(500).send("Password compare Error");
+      }
+      if (same) {
+        res.json({
+          username: results[0].username,
+          id: results[0].id,
+        });
+      } else {
+        res.status(401).send("Wrong password");
+      }
+    });
+  });
+}
 
 async function hashPassword(req, res) {
   const raw = req.params.raw;
