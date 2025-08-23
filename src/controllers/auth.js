697c72cca7788968c163332ba4e552eeba42a8a2
Pattaphon-sr <zenotadeo@gmail.com>
2025-08-23 15:26:36 +0700

    add function hashPassword

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,16 @@
-const { getConnection } = require('../config/db.js');
+const { getConnection } = require("../config/db.js");
+const bcrypt = require("bcrypt");
 
 async function login(req, res) {}
 
-module.exports = { login };
+async function hashPassword(req, res) {
+  const raw = req.params.raw;
+  bcrypt.hash(raw, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).send("Error creating password");
+    }
+    res.send(hash);
+  });
+}
+
+module.exports = { login, hashPassword };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { login } = require('../controllers/auth.js');
+const { login, hashPassword } = require('../controllers/auth.js');
 
 const router = Router();
 
+router.get('/password/:raw', hashPassword);
 router.post('/login', login);
 
 module.exports = router;
