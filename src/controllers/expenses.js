d5bf0b5903ef00b0188e76d4eee00d8f9a8708f1
noobydev546 <donutaaaa19@gmail.com>
2025-08-23 23:23:51 +0700

    Complete delete expense api

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -8,7 +8,19 @@ async function searchItems(req, res) {}
 
 async function createExpense(req, res) {}
 
-async function deleteExpense(req, res) {}
+async function deleteExpense(req, res) {
+  const { id } = req.params;
+  try {
+    const con = await getConnection();
+    const [results] = await con.execute("DELETE FROM `expenses` WHERE id = ?", [id]);
+    if (results.affectedRows === 0) {
+      return res.status(404).send("Expense not found");
+    }
+    res.send("Expense deleted successfully");
+  } catch (err) {
+    res.status(500).send("Database error");
+  }
+}
 
 module.exports = {
   listAll,
