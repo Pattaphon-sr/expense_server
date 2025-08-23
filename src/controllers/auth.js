3c80a814c9670cda14cc9a3bf5738f72e3061bd4
noobydev546 <donutaaaa19@gmail.com>
2025-08-24 00:53:58 +0700

    fix: update database connection port and refactor auth controller for better error handling add: implement createExpense function in expenses controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,31 +3,25 @@ const bcrypt = require("bcrypt");
 
 async function login(req, res) {
   const { username, password } = req.body;
-  const sql = "SELECT * FROM `users` WHERE username = ?";
-
-  const con = await getConnection();
-  
-  con.query(sql, username, (err, results) => {
-    if (err) {
-      return res.status(500).send("Database error");
-    }
-    if (results.length != 1) {
+  const sql = "SELECT * FROM users WHERE username = ?";
+  try {
+    const con = await getConnection();
+    const [results] = await con.execute(sql, [username]);
+    if (results.length !== 1) {
       return res.status(401).send("wrong username");
     }
-    bcrypt.compare(password, results[0].password, (err, same) => {
-      if (err) {
-        return res.status(500).send("Password compare Error");
-      }
-      if (same) {
-        res.json({
-          username: results[0].username,
-          id: results[0].id,
-        });
-      } else {
-        res.status(401).send("Wrong password");
-      }
-    });
-  });
+    const same = await bcrypt.compare(password, results[0].password);
+    if (same) {
+      res.json({
+        username: results[0].username,
+        id: results[0].id,
+      });
+    } else {
+      res.status(401).send("Wrong password");
+    }
+  } catch (err) {
+    res.status(500).send("Database error");
+  }
 }
 
 async function hashPassword(req, res) {
